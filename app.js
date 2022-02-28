028123fcf48f856e9cc1634e9df64a93689ffa53
DonaldBowler44 <96385571+DonaldBowler44@users.noreply.github.com>
2022-02-28 10:39:00 -0600

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
+mongoose.connect("LOCALHOST/DATABASENAME", {useNewUrlParser: true});
 
 const itemsSchema = {
   name: String
